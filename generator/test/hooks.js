56b972cea061bef17efdb64be2672fe75387b81c
Pavel Gruba <pavel.gruba@devexpress.com>
2019-11-22 13:52:22 +0300

    Change path in test hooks

--- generator/test/hooks.js
+++ generator/test/hooks.js
@@ -1,5 +1,5 @@
 const compile = require("../component-compiler")
 
 before(function() {
-    compile(`${__dirname}\\declarations`, `${__dirname}/componentFactory`);
+    compile(`${__dirname}/declarations`, `${__dirname}/componentFactory`);
 });
