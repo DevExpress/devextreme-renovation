39b690bc615fd2fb47f17fa5e71ec447a2de55d6
Andrey Churkin <alam17@mail.ru>
2020-03-26 13:26:49 +0300

    Add webpack dev server for react functional tests (#31)
    * Add webpack dev server for react functional tests
    
    * Update tsconfig.json
    
    Co-authored-by: Andrey Churkin <andrey.churkin@andreys-mbp.corp.devexpress.com>
    Co-authored-by: Roman Simionov <roman-simionov@users.noreply.github.com>

---
+++ generator/test/functional/platforms/loader.js
@@ -0,0 +1,29 @@
+const { getOptions } = require('loader-utils');
+const { compileCode } = require('../../../build/component-compiler');
+const reactGenerator = require('../../../build/react-generator').default;
+const agularGenerator = require('../../../build/angular-generator').default;
+//const vueGenerator = require('../../../build/vue-generator').default;
+
+module.exports = function(source) {
+  const { platform } = getOptions(this);
+  let generator = null;
+
+  switch(platform) {
+    case 'react':
+      generator = reactGenerator;
+      break;
+    case 'angular':
+      generator = agularGenerator;
+      break;
+    // case 'vue':
+    //   generator = vueGenerator;
+    //   break;
+    default:
+      throw new Error('Invalid platform');
+  }
+
+  return compileCode(generator, source, {
+    path: this.resourcePath,
+    dirname: 'dirname'
+  });
+}
