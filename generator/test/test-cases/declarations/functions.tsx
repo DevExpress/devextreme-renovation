82465f4b5ac7e76ba0eb2bdc29faf8b6b0478b10
Pavel Gruba <pavel.gruba@devexpress.com>
2020-04-21 16:24:34 +0300

    Fix 65

--- generator/test/test-cases/declarations/functions.tsx
+++ generator/test/test-cases/declarations/functions.tsx
@@ -4,12 +4,21 @@ function namedFunction() {
 const b = function (a) { return a };
 let c = (a:number, b:number):null => null;
 
-export function plus(a:number=45, b?:number):number { 
+export function plus(a:number=45, c: any, b?:number):number { 
     (namedFunction());
     (c(2,3));
 
     (namedFunction())
     (c(2,3))
+
+    const {p} = c;
+    (p as any).SomeMethod({})
+
+    if(p != null) {
+        a = p + 10;
+    }
+    (p as any).SomeMethod({})
+
     return a + b;
 }
 
