1e278b2a8e306b9fc9dcda274973b0c1b615ffd8
maximkudriavtsev <maxim_71rus_wow@mail.ru>
2020-04-23 11:27:04 +0300

    Fix test

--- generator/test/test-cases/expected/angular/globals-in-template.tsx
+++ generator/test/test-cases/expected/angular/globals-in-template.tsx
@@ -15,7 +15,7 @@ import { CommonModule } from "@angular/common"
     template: `<div [class]="('dx'+'c1'+'c2'+'c3')"></div>`
 })
 export default class WidgetWithGlobals extends WidgetProps {
-    get restAttributes() {
+    get __restAttributes() {
         return {}
     }
 
