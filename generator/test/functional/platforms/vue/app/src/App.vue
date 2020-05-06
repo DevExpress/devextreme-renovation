265ba342c12084b5fda5adfb644047da45ad41c3
Roman Simionov <>
2020-05-06 12:04:16 +0300

    Add Vue Config for functional tests

---
+++ generator/test/functional/platforms/vue/app/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+ <div>
+    <dx-app
+      :width="100"
+      :height="100"
+    >
+      <div>Click!!</div>
+    </dx-app>
+ </div>
+</template>
+<script>
+
+import DxApp from "../../../../components/button.tsx";
+
+export default {
+  components: {
+    DxApp
+  },
+
+  data() {
+   
+  },
+  methods: {
+   
+  }
+};
+</script>
