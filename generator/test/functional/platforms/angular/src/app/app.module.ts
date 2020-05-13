93c1b373c6dc93575d2b4c7734434da2e201f933
roman-simionov <roman-simionov@users.noreply.github.com>
2020-05-13 13:56:55 +0300

    Functional Tests: Import App module in angular application

--- generator/test/functional/platforms/angular/src/app/app.module.ts
+++ generator/test/functional/platforms/angular/src/app/app.module.ts
@@ -1,33 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import DxApp from '../../../../components/app';
-import DxSimple from '../../../../components/simple';
-import DxButton from '../../../../components/button';
-import DxButtonWithState from '../../../../components/state';
-import DxComponentWithSpread from '../../../../components/spread-attributes';
-import DxVisibilityChange from '../../../../components/change-visibility';
-import DxVisibilityChangeProp from '../../../../components/change-visibility-prop';
-import ComponentWithRest from '../../../../components/rest-attributes';
-import CallMethodInGetter from '../../../../components/call-method-in-getter';
-import ComponentWithFragment from '../../../../components/component-with-fragment';
+import { DxAppModule } from '../../../../components/app';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DxSimple,
-    DxApp,
-    DxButton,
-    DxButtonWithState,
-    DxComponentWithSpread,
-    DxVisibilityChange,
-    DxVisibilityChangeProp,
-    ComponentWithRest,
-    CallMethodInGetter,
-    ComponentWithFragment
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DxAppModule
   ],
   providers: [],
   bootstrap: [AppComponent]
