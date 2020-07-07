bab6a668a1c0daacc75a1d5c94ecfb03f713a892
roman-simionov <roman-simionov@users.noreply.github.com>
2020-07-07 13:59:00 +0300

    move getTsConfig in typescript-utils

--- generator/webpack-loader.ts
+++ generator/webpack-loader.ts
@@ -8,28 +8,7 @@ import path from "path";
 import { GeneratorOptions } from "./base-generator/types";
 import { loader } from "webpack";
 import { compileCode } from "./component-compiler";
-
-function getTsConfig(filename: string) {
-  const { config, error } = ts.readConfigFile(filename, ts.sys.readFile);
-  if (error) {
-    return {};
-  }
-  let baseConfig: any = {};
-  if (config.extends) {
-    baseConfig = getTsConfig(
-      path.resolve(path.dirname(filename), config.extends)
-    );
-  }
-  return {
-    ...baseConfig,
-    ...config,
-    compilerOptions: {
-      ...baseConfig.compilerOptions,
-      ...config.compilerOptions,
-      sourceMap: false,
-    },
-  };
-}
+import { getTsConfig } from "./utils/typescript-utils";
 
 export default function (this: loader.LoaderContext, source: string) {
   const {
