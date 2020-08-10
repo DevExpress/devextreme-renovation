78d0ab1cfd84b23fc1606d701fa18d724874f753
Pavel Gruba <pavel.gruba@devexpress.com>
2020-08-11 02:44:43 +0300

    Add missed files

---
+++ generator/test/test-cases/expected/vue/import-component-named.vue
@@ -0,0 +1,32 @@
+<template>
+  <Widget :prop="true" />
+</template>
+<script>
+import { DxWidget as Widget } from "./export-named";
+
+const ChildInput = {
+  height: {
+    type: Number,
+    default() {
+      return 10;
+    },
+  },
+};
+export const DxChild = {
+  components: {
+    Widget,
+  },
+  props: ChildInput,
+  computed: {
+    __restAttributes() {
+      return {};
+    },
+    props() {
+      return {
+        height: this.height,
+      };
+    },
+  },
+};
+export default DxChild;
+</script>
