fc93567182be6634a72a543bd4c3756c09f2a057
Andrey Ignatovskiy <lazylahtak@yandex.ru>
2020-09-10 12:43:41 +0300

    Make possible to work our components with enzyme

---
+++ generator/test/test-cases/declarations/src/create-component-new.tsx
@@ -0,0 +1,6 @@
+import { WidgetWithProps } from "./dx-widget-with-props";
+
+const WithEmptyProps = new WidgetWithProps({});
+const WithProps = new WidgetWithProps({ value: "1" });
+const JsxEmpty = <WidgetWithProps />;
+const JsxWithProps = <WidgetWithProps value={"1"} key={0} ref={{} as any} />;
