b8da5c83f61a798c236d8704a2882385278ea6f2
roman-simionov <roman-simionov@users.noreply.github.com>
2020-12-22 13:15:20 +0300

    rewrite native-component test

--- generator/e2e/platforms/inferno/app/src/pages/native-components.js
+++ generator/e2e/platforms/inferno/app/src/pages/native-components.js
@@ -1,4 +1,5 @@
 import { createElement as h } from "inferno-create-element";
+import { Component } from "inferno";
 
 import ButtonWithTemplate from "../../../../../components/button-with-template";
 import Counter from "../../../../../components/counter";
@@ -9,38 +10,54 @@ const buttonTemplate = ({ text }) => (
   <div style={{ border: "1px solid blue" }}>{text + "!"}</div>
 );
 
-export default () => {
-  const [counterValue, counterValueChange] = [10, () => {}];
-  return (
-    <div>
-      <ButtonWithTemplate
-        text={"With Template"}
-        template={buttonTemplate}
-      ></ButtonWithTemplate>
+export default class NativeComponents extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counterValue: 10,
+    };
 
-      <ButtonWithTemplate text={"Without Template"}></ButtonWithTemplate>
+    this.counterValueChange = this.counterValueChange.bind(this);
+  }
 
-      <form>
-        <Counter
-          id="counter-control"
-          value={counterValue}
-          valueChange={counterValueChange}
-        ></Counter>
-      </form>
-      <div id="counter-form-value">{counterValue}</div>
+  counterValueChange(e) {
+    this.setState({
+      counterValue: e,
+    });
+  }
 
-      <Nested
-        rows={[
-          { cells: [{ gridData: "cell11" }, "cell12"] },
-          { cells: ["cell21", "cell22"] },
-        ]}
-      ></Nested>
-      <Nested rows={[{ cells: ["cell31", { gridData: "cell32" }] }]}></Nested>
-      <Nested
-        rows={[{ cells: [{ gridData: "cell41" }, { gridData: "cell42" }] }]}
-      ></Nested>
-      <Nested rows={[]}></Nested>
-      <Nested></Nested>
-    </div>
-  );
-};
+  render() {
+    return (
+      <div>
+        <ButtonWithTemplate
+          text={"With Template"}
+          template={buttonTemplate}
+        ></ButtonWithTemplate>
+
+        <ButtonWithTemplate text={"Without Template"}></ButtonWithTemplate>
+
+        <form>
+          <Counter
+            id="counter-control"
+            value={this.state.counterValue}
+            valueChange={this.counterValueChange}
+          ></Counter>
+        </form>
+        <div id="counter-form-value">{this.state.counterValue}</div>
+
+        <Nested
+          rows={[
+            { cells: [{ gridData: "cell11" }, "cell12"] },
+            { cells: ["cell21", "cell22"] },
+          ]}
+        ></Nested>
+        <Nested rows={[{ cells: ["cell31", { gridData: "cell32" }] }]}></Nested>
+        <Nested
+          rows={[{ cells: [{ gridData: "cell41" }, { gridData: "cell42" }] }]}
+        ></Nested>
+        <Nested rows={[]}></Nested>
+        <Nested></Nested>
+      </div>
+    );
+  }
+}
