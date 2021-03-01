345be79d3f1d4196f4d0caecc13b494b48b0d89b
Andrey Ignatovskiy <43685423+LazyLahtak@users.noreply.github.com>
2021-03-01 08:29:34 +0300

    Add inferno-common package

---
+++ packages/inferno-common/src/base_component.ts
@@ -0,0 +1,41 @@
+import { Component } from "inferno";
+import { InfernoEffect } from "./effect";
+import { InfernoEffectHost } from "./effect_host";
+
+export class InfernoComponent<P = {}, S = {}> extends Component<P, S> {
+  _effects: InfernoEffect[] = [];
+
+  createEffects(): InfernoEffect[] {
+    return [];
+  }
+
+  updateEffects() {}
+
+  componentWillMount() {
+    InfernoEffectHost.lock();
+  }
+
+  componentWillUpdate() {
+    InfernoEffectHost.lock();
+  }
+
+  componentDidMount() {
+    InfernoEffectHost.callbacks.push(
+      () => (this._effects = this.createEffects())
+    );
+    InfernoEffectHost.callEffects();
+  }
+
+  componentDidUpdate() {
+    InfernoEffectHost.callbacks.push(() => this.updateEffects());
+    InfernoEffectHost.callEffects();
+  }
+
+  destroyEffects() {
+    this._effects.forEach((e) => e.dispose());
+  }
+
+  componentWillUnmount() {
+    this.destroyEffects();
+  }
+}
---
+++ packages/inferno-common/src/index.ts
@@ -0,0 +1,6 @@
+export * from "./base_component";
+export * from "./create_context";
+export * from "./effect";
+export * from "./effect_host";
+export * from "./portal";
+export * from "./ref_object";
