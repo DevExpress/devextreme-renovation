b85e0633951c0752242c657aa28a0b74e4e28a38
Andrey Ignatovskiy <43685423+LazyLahtak@users.noreply.github.com>
2021-03-03 10:05:04 +0300

    Inferno: prevent excess re-renders (#586)

--- generator/modules/inferno/base_component.ts
+++ generator/modules/inferno/base_component.ts
@@ -2,8 +2,39 @@ import { Component } from "inferno";
 import { InfernoEffect } from "./effect";
 import { InfernoEffectHost } from "./effect_host";
 
+const areObjectsEqual = (firstObject: any, secondObject: any) => {
+  const bothAreObjects =
+    firstObject instanceof Object && secondObject instanceof Object;
+  if (!bothAreObjects) {
+    return firstObject === secondObject;
+  }
+
+  const firstObjectKeys = Object.keys(firstObject);
+  const secondObjectKeys = Object.keys(secondObject);
+  if (firstObjectKeys.length !== secondObjectKeys.length) {
+    return false;
+  }
+
+  const hasDifferentElement = firstObjectKeys.some(
+    (key) => firstObject[key] !== secondObject[key]
+  );
+  return !hasDifferentElement;
+};
+
 export class InfernoComponent<P = {}, S = {}> extends Component<P, S> {
   _effects: InfernoEffect[] = [];
+  _pendingContext: any = this.context;
+
+  componentWillReceiveProps(_: any, context: any) {
+    this._pendingContext = context ?? {};
+  }
+  shouldComponentUpdate(nextProps: P, nextState: S) {
+    return (
+      !areObjectsEqual(this.props, nextProps) ||
+      !areObjectsEqual(this.state, nextState) ||
+      !areObjectsEqual(this.context, this._pendingContext)
+    );
+  }
 
   createEffects(): InfernoEffect[] {
     return [];
