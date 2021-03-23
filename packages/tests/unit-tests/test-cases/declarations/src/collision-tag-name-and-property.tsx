bdcf910d5a9558b5b100f8d6891dbcf2bcbbc0b3
Andrey Ignatovskiy <43685423+LazyLahtak@users.noreply.github.com>
2021-03-23 10:41:29 +0300

    Fix tests

--- packages/tests/unit-tests/test-cases/declarations/src/collision-tag-name-and-property.tsx
+++ packages/tests/unit-tests/test-cases/declarations/src/collision-tag-name-and-property.tsx
@@ -3,7 +3,7 @@ import {
   ComponentBindings,
   JSXComponent,
   OneWay,
-} from "../../../../component_declaration/common";
+} from "@devextreme-generator/declarations";
 
 export const viewFunction = ({
   computedProps,
