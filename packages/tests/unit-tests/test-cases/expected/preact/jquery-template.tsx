27c3d056ba2a573dc540d26032fb1cd92f14e99b
Andrey Ignatovskiy <43685423+LazyLahtak@users.noreply.github.com>
2021-05-12 18:01:41 +0300

    Remove some generated code for jQuery templates (#675)

--- packages/tests/unit-tests/test-cases/expected/preact/jquery-template.tsx
+++ packages/tests/unit-tests/test-cases/expected/preact/jquery-template.tsx
@@ -3,25 +3,6 @@ import BaseComponent from "../../../../jquery-helpers/jquery_base_component";
 import WidgetComponent from "../../../../jquery-template";
 
 export default class Widget extends BaseComponent {
-  getProps() {
-    const props = super.getProps();
-    props.template = this._componentTemplates.template;
-    props.anotherTemplate = this._componentTemplates.anotherTemplate;
-    return props;
-  }
-
-  _optionChanged(option) {
-    const { name, value } = option;
-    if (name === "template") {
-      this._componentTemplates.template = this._createTemplateComponent(value);
-    }
-    if (name === "anotherTemplate") {
-      this._componentTemplates.anotherTemplate =
-        this._createTemplateComponent(value);
-    }
-    super._optionChanged(option);
-  }
-
   get _propsInfo() {
     return {
       twoWay: [],
