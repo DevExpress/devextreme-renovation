25875bd48fae18b728013e84ef3275526b09f5ba
SelyankinFyodor <fedkahuligan@gmail.com>
2021-09-22 10:46:34 +0300

    between_props.ts file extension

--- packages/tests/unit-tests/test-cases/declarations/src/between_props.tsx
+++ packages/tests/unit-tests/test-cases/declarations/src/between_props.tsx
@@ -1,8 +1,8 @@
 import {
-    ComponentBindings, JSXTemplate, OneWay, Template,
+    ComponentBindings, JSXTemplate, Template,
   } from '@devextreme-generator/declarations';
   import { Child, ChildProps } from './child';
-  
+
   @ComponentBindings()
   export class BetweenProps {
     @Template() childTemplate: JSXTemplate<ChildProps> = Child;
