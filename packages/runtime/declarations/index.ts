62cd3f75fcc0aa9d1a5fb76543ebdd1231540d10
dxvladislavvolkov <vladislavvolkov@Vladislavs-MacBook-Pro.local>
2021-10-04 16:08:22 +0300

    Fix type errors when compiling

--- packages/runtime/declarations/index.ts
+++ packages/runtime/declarations/index.ts
@@ -1,2 +1,3 @@
-export const renderTemplate = (template: any, model: any, component: any): void => undefined;
-export const hasTemplate = (name: any, props: any, component: any): boolean => false;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const renderTemplate = (_template: any, _model: any, _component: any): void => undefined;
+export const hasTemplate = (_name: any, _props: any, _component: any): boolean => false;
