9d569f91259be980c2797135837c42509edac6c8
dxvladislavvolkov <vladislavvolkov@Vladislavs-MacBook-Pro.local>
2021-10-25 17:06:29 +0300

    Fix anonymous template for wrapper components

--- packages/runtime/react/template.tsx
+++ packages/runtime/react/template.tsx
@@ -5,17 +5,19 @@ import * as React from 'react';
 export const renderTemplate = (template: any, model: any, _component?: any): void => {
   template(model, model.container);
 };
-export const hasTemplate = (name: any, props: any, _component?: any): boolean => !!props[name];
-export const getWrapperTemplate = (TemplateProp: any) => (data: any) => {
-  if (typeof TemplateProp !== 'string' && !(TemplateProp instanceof Element)) {
-    const container = data.container ? data.container : data;
-    ReactDOM.render(
-      /* eslint-disable react/jsx-props-no-spreading */
-      <><TemplateProp {...data} /></> as React.ReactElement,
-      container,
-    );
-  }
+export const hasTemplate = (name: any, props: any, _component?: any): boolean => {
+  const value = props[name];
+  return !!value && typeof value !== 'string';
 };
+export const getWrapperTemplate = (TemplateProp: any) => (typeof TemplateProp === 'string' || !!(TemplateProp instanceof Element) ? TemplateProp : (data: any) => {
+  const container = data.container ? data.container : data;
+  ReactDOM.render(
+    /* eslint-disable react/jsx-props-no-spreading */
+    <><TemplateProp {...data} /></> as React.ReactElement,
+    container,
+  );
+});
+
 export const getTemplate = (TemplateProp: any, RenderProp: any, ComponentProp: any) => (
   (TemplateProp && (TemplateProp.defaultProps ? (props: any) => <TemplateProp {...props} /> : TemplateProp))
     || (RenderProp
