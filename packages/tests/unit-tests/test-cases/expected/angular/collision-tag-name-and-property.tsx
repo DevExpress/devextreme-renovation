de6c4c8ab9e6c460e87ca6fb8dd8c4722fd2a6d1
SelyankinFyodor <fedkahuligan@gmail.com>
2021-11-18 16:38:31 +0300

    unit test fix

--- packages/tests/unit-tests/test-cases/expected/angular/collision-tag-name-and-property.tsx
+++ packages/tests/unit-tests/test-cases/expected/angular/collision-tag-name-and-property.tsx
@@ -20,13 +20,11 @@ import { CommonModule } from "@angular/common";
   selector: "g [TextSvgElement]",
   changeDetection: ChangeDetectionStrategy.OnPush,
   inputs: ["text"],
-  template: `<ng-template #widgetTemplate
-    ><svg:text>
-      <svg:ng-container *ngIf="![].length">
-        {{ __computedProps.text }}
-      </svg:ng-container>
-    </svg:text></ng-template
-  >`,
+  template: `<svg:text>
+    <svg:ng-container *ngIf="![].length">
+      {{ __computedProps.text }}
+    </svg:ng-container>
+  </svg:text>`,
 })
 export class TextSvgElement extends TextSvgElementProps {
   get __computedProps(): TextSvgElementProps {
