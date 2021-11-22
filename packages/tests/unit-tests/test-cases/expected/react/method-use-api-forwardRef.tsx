8d4e2f4d5bc1df7510b391d00050d61ca926c9d1
ivanblinov2k17 <ivanblinov2015@mail.ru>
2021-11-22 10:21:12 +0300

    React Forward Ref Api method call fix  (#808)
    * React: Forward ref with method types fix
    
    * fix
    
    * removed redundant test files

---
+++ packages/tests/unit-tests/test-cases/expected/react/method-use-api-forwardRef.tsx
@@ -0,0 +1,62 @@
+import { MutableRefObject } from "react";
+import BaseWidget from "./method";
+export const viewFunction = ({
+  props: { forwardedRef },
+}: Child1Component): any => <BaseWidget ref={forwardedRef} />;
+
+export declare type Child1ComponentPropsType = {
+  forwardedRef: any;
+};
+export const Child1ComponentProps: Child1ComponentPropsType =
+  {} as any as Child1ComponentPropsType;
+import { WidgetRef as BaseWidgetRef } from "./method";
+import * as React from "react";
+import { useCallback, useEffect, useRef } from "react";
+
+declare type RestProps = {
+  className?: string;
+  style?: { [name: string]: any };
+  key?: any;
+  ref?: any;
+};
+interface Child1Component {
+  props: typeof Child1ComponentProps & RestProps;
+  someGetter: Partial<typeof Child1ComponentProps>;
+  restAttributes: RestProps;
+  forwardedRef: any;
+}
+
+export function Child1Component(
+  props: typeof Child1ComponentProps & RestProps
+) {
+  const forwardedRef: MutableRefObject<BaseWidgetRef | null> =
+    useRef<BaseWidgetRef>(null);
+
+  const __someGetter = useCallback(function __someGetter(): Partial<
+    typeof Child1ComponentProps
+  > {
+    return {};
+  },
+  []);
+  const __restAttributes = useCallback(
+    function __restAttributes(): RestProps {
+      const { forwardedRef, ...restProps } = props;
+      return restProps;
+    },
+    [props]
+  );
+  useEffect(() => {
+    console.log(props.forwardedRef.current!.getSize());
+  }, []);
+
+  return viewFunction({
+    props: { ...props },
+    forwardedRef: forwardedRef,
+    someGetter: __someGetter(),
+    restAttributes: __restAttributes(),
+  });
+}
+
+export default Child1Component;
+
+Child1Component.defaultProps = Child1ComponentProps;
