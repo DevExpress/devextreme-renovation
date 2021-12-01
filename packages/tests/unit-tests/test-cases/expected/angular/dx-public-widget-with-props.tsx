892e0b7b687d3fb886a93b0c74c11713e0302654
ivanblinov2k17 <ivanblinov2015@mail.ru>
2021-12-01 10:55:44 +0300

    changed new test

--- packages/tests/unit-tests/test-cases/expected/angular/dx-public-widget-with-props.tsx
+++ packages/tests/unit-tests/test-cases/expected/angular/dx-public-widget-with-props.tsx
@@ -28,6 +28,7 @@ import { CommonModule } from "@angular/common";
   template: `<div>{{ optionalValue || value }}</div>`,
 })
 export class PublicWidgetWithProps extends WidgetWithPropsInput {
+  propsDefaults = new WidgetWithPropsInput();
   doSomething(): any {}
   get __restAttributes(): any {
     return {};
@@ -37,6 +38,15 @@ export class PublicWidgetWithProps extends WidgetWithPropsInput {
       if (this.changeDetection && !(this.changeDetection as ViewRef).destroyed)
         this.changeDetection.detectChanges();
     });
+  }
+
+  ngOnChanges(changes: { [name: string]: any }) {
+    if (changes["value"] && changes["value"].currentValue === undefined) {
+      this.value = this.propsDefaults.value;
+    }
+    if (changes["number"] && changes["number"].currentValue === undefined) {
+      this.number = this.propsDefaults.number;
+    }
   }
 
   _onClick: any;
