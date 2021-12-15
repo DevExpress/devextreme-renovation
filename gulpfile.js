6256ad2ae4a0775e3672fd93cdce210345607a52
dxvladislavvolkov <vladislavvolkov@vladislavs-mbp.corp.devexpress.com>
2021-12-15 14:19:02 +0300

    Refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 const gulp = require('gulp');
 const run = require('gulp-run');
+const path = require('path');
 
-const installTo = process.argv[process.argv.indexOf("--to") + 1];
-const relativePath = installTo.match(/(\.*\/)+/g)[0];
+const installTo = path.join(__dirname ,process.argv[process.argv.length - 1]);
+const relativePath = path.relative(installTo, __dirname);
 const packages = [
   'core-generator',
   'angular-generator',
@@ -16,7 +17,7 @@ const packages = [
 
 const prepareInstallTask = (installTo, relativePath) => {
   packages.forEach((package) => {
-    gulp.task(`install:${package}`, () => run(`npm i ${relativePath}devextreme-renovation/packages/${package}/package.tgz`, { cwd: installTo }).exec());
+    gulp.task(`install:${package}`, () => run(`npm i ${relativePath}/packages/${package}/package.tgz`, { cwd: installTo }).exec());
   });
 };
 
