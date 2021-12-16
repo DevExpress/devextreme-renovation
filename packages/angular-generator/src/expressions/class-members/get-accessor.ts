5e00b9fd6b8944796567fab84ed9aa6a6b537c93
ivanblinov2k17 <ivanblinov2015@mail.ru>
2021-12-16 11:21:43 +0300

    removed angular dep in inferno-generator (#843)

--- packages/angular-generator/src/expressions/class-members/get-accessor.ts
+++ packages/angular-generator/src/expressions/class-members/get-accessor.ts
@@ -1,60 +1,10 @@
 import {
-  ArrowFunction,
-  Binary,
-  Block,
-  Call,
   GetAccessor as BaseGetAccessor,
   Identifier,
-  Paren,
-  ReturnStatement,
-  SimpleExpression,
-  SyntaxKind,
-  TypeExpression,
-  Expression,
   toStringOptions,
+  compileGetterCache,
 } from '@devextreme-generator/core';
 
-export const compileGetterCache = (
-  name: Identifier,
-  type: TypeExpression | string | undefined,
-  body: Block,
-  isProvider: boolean | undefined,
-  needToHandleProvider = true,
-): Expression[] => {
-  const cacheAccess = `this.__getterCache["${name.toString()}"]`;
-  const setCacheExpression = new Binary(
-    new SimpleExpression(cacheAccess),
-    SyntaxKind.EqualsToken,
-    new Call(
-      new Paren(
-        new ArrowFunction(
-          [],
-          [],
-          [],
-          type,
-          SyntaxKind.EqualsGreaterThanToken,
-          new Block(body.statements, false),
-          {},
-        ),
-      ),
-      undefined,
-    ),
-  );
-  const returnExpression = isProvider && needToHandleProvider
-    ? new Binary(
-      new SimpleExpression(`this.${name}Provider.value`),
-      SyntaxKind.EqualsToken,
-      setCacheExpression,
-    )
-    : setCacheExpression;
-  return [
-    new SimpleExpression(`
-                if(${cacheAccess}!==undefined){
-                    return ${cacheAccess};
-                }`),
-    new ReturnStatement(returnExpression),
-  ];
-};
 export class GetAccessor extends BaseGetAccessor {
   get canBeDestructured() {
     if (
