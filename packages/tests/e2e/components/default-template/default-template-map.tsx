8baaae9847f3f87f136dbb5c0ee2109dcff488a4
ivanblinov2k17 <ivanblinov2015@mail.ru>
2022-01-24 12:53:57 +0300

    Fix: Default properties for templates in Angular work incorrectly

---
+++ packages/tests/e2e/components/default-template/default-template-map.tsx
@@ -0,0 +1,49 @@
+import {
+  Component,
+  ComponentBindings,
+  JSXComponent,
+  JSXTemplate,
+  Template,
+} from '@devextreme-generator/declarations';
+import { SimpleSlot } from '../slot/simple-slot';
+import { SecondComponent, SecondComponentProps } from './second-component';
+
+export interface ComplexData {
+  a: string;
+}
+
+export const viewFunction = ({
+  complexData,
+  props: {
+    secondComponentTemplate: SecondComponentTemplate,
+  },
+}: FirstComponent) => (
+  <div>
+    {complexData.map((data1) => (
+      <SimpleSlot>
+        {data1.map(({
+          data,
+        }) => <SecondComponentTemplate complexData={data} />)}
+      </SimpleSlot>
+    ))}
+  </div>
+);
+
+@ComponentBindings()
+export class FirstComponentProps {
+  @Template() secondComponentTemplate: JSXTemplate<SecondComponentProps> = SecondComponent;
+}
+
+@Component({
+  defaultOptionRules: null,
+  jQuery: { register: true },
+  view: viewFunction,
+})
+
+export default class FirstComponent extends JSXComponent(FirstComponentProps) {
+  get complexData(): {
+    data: ComplexData | undefined;
+  }[][] {
+    return [[{ data: undefined }]];
+  }
+}
---
+++ packages/tests/e2e/components/default-template/second-component.tsx
@@ -0,0 +1,27 @@
+import {
+  Component,
+  ComponentBindings,
+  JSXComponent,
+  OneWay,
+} from '@devextreme-generator/declarations';
+export interface ComplexData {
+  a: string;
+}
+
+export const viewFunction = (viewModel: SecondComponent): JSX.Element => (
+  <div id="defaultTemplateMap">
+    {viewModel.props.complexData!.a}
+  </div>
+);
+
+@ComponentBindings()
+export class SecondComponentProps {
+  @OneWay() complexData?: ComplexData = { a: 'Default value with map' };
+}
+
+@Component({
+  defaultOptionRules: null,
+  view: viewFunction,
+})
+
+export class SecondComponent extends JSXComponent(SecondComponentProps) {}
