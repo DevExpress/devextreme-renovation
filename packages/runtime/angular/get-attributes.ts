6072674b9f3b457199dfb5c79400a175a92bcbe3
volnyagin <vconst@users.noreply.github.com>
2022-02-10 20:42:37 +0300

    Split getAttributes helper method

--- packages/runtime/angular/get-attributes.ts
+++ packages/runtime/angular/get-attributes.ts
@@ -1,29 +1,42 @@
 import { ElementRef } from '@angular/core';
 
-export function getAttributes(element: ElementRef<HTMLElement>): Record<string, unknown> {
-  const attributes: Record<string, string> = {};
-  Array.from(
-    element.nativeElement.attributes,
-  ).forEach(({ name, value }) => {
-    if (name.startsWith('ng-reflect')) return;
-    attributes[name] = value;
+function getStyleObject(styleText: string): Record<string, unknown> {
+  const style: Record<string, unknown> = {
+    toString: () => styleText,
+  };
+  styleText.split(';').forEach((definition) => {
+    const [name, value] = definition.split(':');
+    if (name && value) {
+      style[name.trim()] = value.trim();
+    }
   });
+  return style;
+}
+
+function processStyleAttribute(attributes: Record<string, string>): Record<string, unknown> {
   if (attributes.style) {
     const styleText = attributes.style.replace(/display: contents[;]?/, '');
 
-    if (styleText) {
-      const style: Record<string, unknown> = {
-        toString: () => styleText,
-      };
-      styleText.split(';').forEach((definition) => {
-        const [name, value] = definition.split(':');
-        if (name && value) {
-          style[name.trim()] = value.trim();
-        }
-      });
-      return { ...attributes, style };
+    if (!styleText) {
+      delete attributes.style;
+      return attributes;
     }
-    delete attributes.style;
+
+    const style = getStyleObject(styleText);
+    return { ...attributes, style };
   }
+
   return attributes;
+}
+
+export function getAttributes(element: ElementRef<HTMLElement>): Record<string, unknown> {
+  const attributes: Record<string, string> = {};
+  Array
+    .from(element.nativeElement.attributes)
+    .filter(({ name }) => !name.startsWith('ng-reflect'))
+    .forEach(({ name, value }) => {
+      attributes[name] = value;
+    });
+
+  return processStyleAttribute(attributes);
 }
