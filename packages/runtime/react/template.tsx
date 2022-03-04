50c7d0b6f3e0bfd9c75d61dd9b149e572a925726
dxvladislavvolkov <vladislavvolkov@Vladislavs-MacBook-Pro.local>
2022-03-04 13:09:30 +0300

    fix

--- packages/runtime/react/template.tsx
+++ packages/runtime/react/template.tsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom';
 import * as React from 'react';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const renderTemplate = (template: any, model: any, _component?: any): void => {
+export const renderTemplate = (template: any, model: any, _component?: any): void | string | Element => {
   const TemplateProp = template;
   const container = model.container ? model.container : model.item;
 
