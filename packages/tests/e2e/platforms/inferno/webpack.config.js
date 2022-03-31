b2da70c93ebc9d66045957c100f443cf3076bd3e
MikeVitik <vitikm@gmail.com>
2022-03-31 17:25:59 +0400

    fix missing babel options

--- packages/tests/e2e/platforms/inferno/webpack.config.js
+++ packages/tests/e2e/platforms/inferno/webpack.config.js
@@ -80,9 +80,7 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
-          options: {
-            presets: babelPresets,
-          },
+          options: babelOptions,
         },
       },
     ],
