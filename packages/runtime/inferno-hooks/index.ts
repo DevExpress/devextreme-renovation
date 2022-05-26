15ce1d19db37c346ac0b4a6f2a48e244621c7b7f
ivanblinov2k17 <ivanblinov2015@mail.ru>
2022-05-26 12:08:11 +0400

    missed file

--- packages/runtime/inferno-hooks/index.ts
+++ packages/runtime/inferno-hooks/index.ts
@@ -2,5 +2,4 @@ export * from './create_context';
 export * from './hooks';
 export * from './base-hooks-component';
 export * from './re_render';
-export * from './portal';
 export * from './ref-object';
