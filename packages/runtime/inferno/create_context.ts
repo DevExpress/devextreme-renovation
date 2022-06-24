fd1ab786b769fdc5e09ab7c5a767913f5bc411f4
ivanblinov2k17 <ivanblinov2015@mail.ru>
2022-06-24 11:33:44 +0400

    Inferno context rework to match with inferno-hooks

--- packages/runtime/inferno/create_context.ts
+++ packages/runtime/inferno/create_context.ts
@@ -1,3 +1,26 @@
-export const createContext = function <T>(defaultValue: T) {
-  return defaultValue;
+import { Component } from 'inferno';
+
+let contextId = 0;
+export const createContext = function<T>(defaultValue: T): { id: number,
+  Provider: any,
+  defaultValue: unknown } {
+  const id = contextId++;
+
+  return {
+    id,
+    defaultValue,
+    Provider: class extends Component<{ value: T }> {
+      getChildContext() {
+        return {
+          ...this.context,
+          [id]: this.props.value || (defaultValue as unknown as T),
+        };
+      }
+
+      render() {
+        return this.props.children;
+      }
+    },
+
+  };
 };
