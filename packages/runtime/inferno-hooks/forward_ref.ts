67020284b43cc01c02a1fe5c49f3d2639b7f75db
ivanblinov2k17 <ivanblinov2015@mail.ru>
2022-08-31 14:01:13 +0400

    Up ts (#925)
    * Update typescript version to 4.3
    
    * Update node version on CI
    
    * check ts version on ci
    
    * turn off hoist for ci
    
    * Bring back hoisting and list ts version
    
    * update testcafe version
    
    * Add typescript in main package.json
    
    * downgrade prettier-eslint
    
    * downgrade lint once more
    
    * ls -g
    
    * location global
    
    * up dxlint and resolve deps
    
    * fix lint errors
    
    * removed unused imports from lint config
    
    * Revert "removed unused imports from lint config"
    
    This reverts commit 805f2338222046a1a6087722fbdf0e2334492409.
    
    * use legacy peer deps
    
    * change legacy peer to force
    
    * Up eslint version, remove npmrc force, fix npm warnings
    
    * bring back package-locks
    
    * Add tokens and keywords, turn on importelpers
    
    * print all packages
    
    * move importHelpers to test tsConfig, remove reporters from CI testing
    
    * fix version for tests compartible with sinon.
    
    * fix tsconfig and some types to pass compilation
    
    * Remove printing ts version
    
    Co-authored-by: MikeVitik <vitikm@gmail.com>

--- packages/runtime/inferno-hooks/forward_ref.ts
+++ packages/runtime/inferno-hooks/forward_ref.ts
@@ -6,7 +6,7 @@ import type { RefObject } from './container';
 export function forwardRef<T = Record<string, unknown>, P = Record<string, unknown>>(
   render: (props: T, ref: RefObject<P>) => InfernoElement<T>,
 ): SFC<T> & infernoForwardRefType {
-  const result = infernoForwardRef(render) as SFC<T> & infernoForwardRefType;
+  const result = infernoForwardRef(render) as unknown as SFC<T> & infernoForwardRefType;
   Object.defineProperty(render, 'defaultProps', {
     get: () => result.defaultProps,
   });
