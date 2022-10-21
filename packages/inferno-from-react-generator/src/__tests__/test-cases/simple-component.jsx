a411806123b905e8f51d922024df34ed9993a515
ivanblinov2k17 <ivanblinov2015@mail.ru>
2022-10-21 17:02:06 +0400

    Jest config in monorepo style (#934)
    * jest config in monorepo style
    
    * Add test for inferno-from-react, update typescript
    
    * Upd node & package-lock
    
    * Update typescript to 4.7
    
    * upd package-locks
    
    * update package-locks
    
    * fix pkg lock
    
    * some progress related to fixing build
    
    * build fixed

--- packages/inferno-from-react-generator/src/__tests__/test-cases/simple-component.jsx
+++ packages/inferno-from-react-generator/src/__tests__/test-cases/simple-component.jsx
@@ -1,6 +1,6 @@
 const simpleComponentProps = { prop: 1 };
 //* Component={"name":"SimpleComponent", "jQueryRegistered":"true"}
 export function SimpleComponent(_props) {
-    return "content";
+  return 'content';
 }
 SimpleComponent.defaultProps = simpleComponentProps;
