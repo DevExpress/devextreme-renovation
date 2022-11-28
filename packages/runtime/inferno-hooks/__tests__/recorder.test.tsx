5b6aea546c82cd5c86c9c8e431685b70c7a49614
MikeVitik <vitikm@gmail.com>
2022-11-28 12:58:29 +0400

    fix review issue

--- packages/runtime/inferno-hooks/__tests__/recorder.test.tsx
+++ packages/runtime/inferno-hooks/__tests__/recorder.test.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { createRecorder } from '../recorder';
 
 jest.mock('../container');
@@ -12,32 +13,90 @@ describe('Recorder', () => {
       };
       const component = { props } as any;
       const recorder = createRecorder(component);
-      expect(recorder.shouldComponentUpdate(props, component.state, {})).toBe(true);
+      const shouldUpdate = recorder.shouldComponentUpdate(
+        props,
+        component.state,
+        {},
+      );
+
+      expect(shouldUpdate).toBe(true);
     });
-    
+
+    test('an impure component should be updated if the props are changed.', () => {
+      const props = {
+        renderProps: {},
+        renderFn: jest.fn(),
+        pure: false,
+      };
+      const component = { props } as any;
+      const recorder = createRecorder(component);
+      const shouldUpdate = recorder.shouldComponentUpdate(
+        { ...props, renderProps: { newProps: 1 } },
+        component.state,
+        {},
+      );
+
+      expect(shouldUpdate).toBe(true);
+      expect(props.renderFn).toBeCalled();
+    });
+
+    test('an impure component should be updated if the state is changed.', () => {
+      const props = {
+        renderProps: {},
+        renderFn: jest.fn(),
+        pure: false,
+      };
+      const component = { props } as any;
+      const recorder = createRecorder(component);
+      const shouldUpdate = recorder.shouldComponentUpdate(
+        props,
+        { newStatePorps: 1 },
+        {},
+      );
+
+      expect(shouldUpdate).toBe(true);
+      expect(props.renderFn).toBeCalled();
+    });
+
     test('a pure component should not be updated if the props and the state are the same', () => {
       const props = {
         renderProps: {},
+        renderFn: jest.fn(),
         pure: true,
       };
       const component = { props } as any;
-      const recorder = createRecorder(component as any);
-      expect(recorder.shouldComponentUpdate(props, component.state, {})).toBe(false);
+      const recorder = createRecorder(component);
+
+      const shouldUpdate = recorder.shouldComponentUpdate(
+        props,
+        component.state,
+        {},
+      );
+
+      expect(shouldUpdate).toBe(false);
+      expect(props.renderFn).not.toBeCalled();
     });
+
     test('a pure component should be updated if the props are changed', () => {
-      const renderProps = { a: 1 };
+      const renderProps = { prop: 1 };
       const props = {
         renderProps,
         renderFn: jest.fn(),
         pure: true,
       };
       const component = { props } as any;
-      const recorder = createRecorder(component as any);
-      expect(
-        recorder.shouldComponentUpdate({ ...props, renderProps: { newProp: true } }, component.state, {}),
-      )
-        .toBe(true);
+      const recorder = createRecorder(component);
+
+      const shouldUpdate = recorder.shouldComponentUpdate(
+        { ...props, renderProps: { newProp: true } },
+        component.state,
+        {},
+      );
+
+      expect(shouldUpdate).toBe(true);
+      expect(props.renderFn).toBeCalled();
     });
+
     test('a pure component should be updated if the state is changed', () => {
       const props = {
         renderProps: {},
@@ -45,8 +104,16 @@ describe('Recorder', () => {
         pure: true,
       };
       const component = { props } as any;
-      const recorder = createRecorder(component as any);
-      expect(recorder.shouldComponentUpdate(props, { newStateProp: true }, {})).toBe(true);
+      const recorder = createRecorder(component);
+
+      const shouldUpdate = recorder.shouldComponentUpdate(
+        props,
+        { newStateProp: true },
+        {},
+      );
+
+      expect(shouldUpdate).toBe(true);
+      expect(props.renderFn).toBeCalled();
     });
   });
 });
