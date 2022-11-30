f9de86fc21d60707feec892f491fadf4bb4891f9
ilyakhd <14272298+IlyaKhD@users.noreply.github.com>
2022-11-30 13:24:01 +0400

    Fix test variable name

--- packages/inferno-from-react-generator/src/__tests__/react-test-cases/import-without-react.tsx
+++ packages/inferno-from-react-generator/src/__tests__/react-test-cases/import-without-react.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useMemo } from 'react';
 
 //* Component={"name":"DxComponent"}
 function DxComponent() {
-  const x = useEffect.toString() + useMemo.toString();
-  return <div className={x} />;
+  const className = useEffect.toString() + useMemo.toString();
+  return <div className={className} />;
 }
 export { DxComponent };
