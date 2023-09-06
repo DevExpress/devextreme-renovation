c63a59bc25f3b98179af2fc181bfc9db2c005122
Ilya Vinogradov <ilya.vinogradov@devexpress.com>
2023-09-06 14:35:04 +1300

    Add excluded module path patterns for code generation.

--- packages/core-generator/src/code-compiler.ts
+++ packages/core-generator/src/code-compiler.ts
@@ -3,13 +3,35 @@ import ts from 'typescript';
 import { generateFactoryCode } from './factoryCodeGenerator';
 import { GeneratorAPI, GeneratorResult } from './generator-api';
 
+export interface CompileCodeOptions {
+  includeExtraComponents: boolean,
+  createFactoryOnly: boolean,
+  // NOTE: NOTE: With this option we can skip generation of the modules
+  // that paths contains one of substrings passed through this option.
+  excludePathPatterns: string[],
+}
+
+const DEFAULT_COMPILE_CODE_OPTIONS: CompileCodeOptions = {
+  includeExtraComponents: false,
+  createFactoryOnly: false,
+  excludePathPatterns: [],
+};
+
 export function compileCode(
   generator: GeneratorAPI,
   code: string,
   file: { dirname: string; path: string; importedModules?: string[] },
-  includeExtraComponents = false,
-  createFactoryOnly = false,
+  options?: Partial<CompileCodeOptions>,
 ): GeneratorResult[] | string {
+  const {
+    includeExtraComponents,
+    createFactoryOnly,
+    excludePathPatterns,
+  } = {
+    ...DEFAULT_COMPILE_CODE_OPTIONS,
+    ...options,
+  };
+
   const source = ts.createSourceFile(
     file.path,
     code,
@@ -20,6 +42,7 @@ export function compileCode(
     path: file.path,
     dirname: file.dirname,
     importedModules: file.importedModules,
+    excludePathPatterns,
   });
   const codeFactory = generateFactoryCode(ts, source, generator.getPlatform());
 
