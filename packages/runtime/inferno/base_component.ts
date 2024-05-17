b5d054cf9544e1aca9e6506c28de270b5cb97c27
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2024-05-17 17:06:59 +0400

    Update base_component.ts

--- packages/runtime/inferno/base_component.ts
+++ packages/runtime/inferno/base_component.ts
@@ -112,10 +112,8 @@ export class InfernoWrapperComponent<
       const indexInRemoved = el.dxClasses.removed.indexOf(value);
       if (indexInRemoved > -1) {
         el.dxClasses.removed.splice(indexInRemoved, 1);
-      } else {
-        if (!el.dxClasses.added.includes(value)) {
-          el.dxClasses.added.push(value);
-        }
+      } else if (!el.dxClasses.added.includes(value)) {
+        el.dxClasses.added.push(value);
       }
     });
 
@@ -123,10 +121,8 @@ export class InfernoWrapperComponent<
       const indexInAdded = el.dxClasses.added.indexOf(value);
       if (indexInAdded > -1) {
         el.dxClasses.added.splice(indexInAdded, 1);
-      } else {
-        if (!el.dxClasses.removed.includes(value)) {
-          el.dxClasses.removed.push(value);
-        }
+      } else if (!el.dxClasses.removed.includes(value)) {
+        el.dxClasses.removed.push(value);
       }
     });
   }
