9ac4f7f6f630e7ca9a82bf74c8094c83ff75c651
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2024-05-20 08:58:58 +0400

    ButtonGroup: button is displayed as active even if it wasn't clicked (T1222079) (#955)
    * ButtonGroup: button is displayed as active even if it wasn't clicked (T1222079)
    
    * Update base_component.ts
    
    * Update webpack-test-compiler.ts

--- packages/runtime/inferno/base_component.ts
+++ packages/runtime/inferno/base_component.ts
@@ -112,7 +112,7 @@ export class InfernoWrapperComponent<
       const indexInRemoved = el.dxClasses.removed.indexOf(value);
       if (indexInRemoved > -1) {
         el.dxClasses.removed.splice(indexInRemoved, 1);
-      } else {
+      } else if (!el.dxClasses.added.includes(value)) {
         el.dxClasses.added.push(value);
       }
     });
@@ -121,7 +121,7 @@ export class InfernoWrapperComponent<
       const indexInAdded = el.dxClasses.added.indexOf(value);
       if (indexInAdded > -1) {
         el.dxClasses.added.splice(indexInAdded, 1);
-      } else {
+      } else if (!el.dxClasses.removed.includes(value)) {
         el.dxClasses.removed.push(value);
       }
     });
